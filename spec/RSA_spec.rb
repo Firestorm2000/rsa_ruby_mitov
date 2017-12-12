bfa222ef1875087059873d31444888a9ca69fd21
FireStorm2000 <teenwolf849@gmail.com>
2017-12-12 21:50:21 +0200

    ohh pls 3

--- spec/RSA_spec.rb
+++ spec/RSA_spec.rb
@@ -1,2 +1,26 @@
 require 'RSA.rb'
+RSpec.describe RSA do
+	
+	msg = "Hell"
+	rsa = RSA.new(671,131,121)
+	context "basic tests" do
+		it "checks if return n works" do
+			expect(rsa.n).to eq(671)
+		end
 
+		it "checks if return e works" do
+			expect(rsa.e).to eq(131)
+		end
+		
+		it "checks if return d works" do
+			expect(rsa.d).to eq(121)
+		end
+	end
+	
+	context "crypt/decrypt tests" do
+		it "crypt/decrypt works?" do
+			expect(rsa.decrypt(rsa.encrypt(msg))).to eq(msg)
+		end
+
+	end
+end
